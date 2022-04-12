b8543d4b6a25e6358b852c882bc483c3dbe665a3
ahsan00252 <ahsan.00252@gmail.com>
2022-04-12 11:56:55 +0200

    refactor changed

--- src/context/context.js
+++ src/context/context.js
@@ -1,5 +1,4 @@
 import React, { useState, useContext, useEffect } from "react";
-import Usefetch from "../useFetch";
 
 export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
 
@@ -7,7 +6,30 @@ const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
   const [query, setQuery] = useState("spider man");
-  const { isLoading, error, data: movies } = Usefetch(`&s=${query}`);
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState({ show: false, msg: "" });
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchMovies = async (url) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      if (data.Response === "True") {
+        setMovies(data.Search);
+        setError({ show: false, msg: "" });
+      } else {
+        setError({ show: true, msg: data.Error });
+      }
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies(`${API_ENDPOINT}&s=${query}`);
+  }, [query]);
 
   return (
     <AppContext.Provider value={{ isLoading, movies, error, query, setQuery }}>
